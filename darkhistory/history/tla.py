be0d8fd96f9b10186b176dd55b36a043fa82f257
Cara Giovanetti <caragiovanetti@gmail.com>
2021-11-29 09:49:56 -0500

    Update tla.py

--- darkhistory/history/tla.py
+++ darkhistory/history/tla.py
@@ -9,6 +9,115 @@ from scipy.integrate import odeint
 from scipy.integrate import solve_ivp
 from scipy.misc import derivative
 
+from scipy.interpolate import interp1d
+from scipy.integrate import dblquad
+from scipy.integrate import quad
+from scipy.integrate import trapz
+from classy import Class
+import math
+import pickle
+
+# Constants
+TCMB_0 = 2.3482e-4 # in eV
+h = 4.136*1e-15    # in eV s
+hbar = h / (2*np.pi) # in eV s
+c = 3e8 # in m/s
+H0 = 1.43654e-33 # in eV
+Omega_r = 8e-5
+Omega_m = 0.3165
+Omega_Lambda = 1 - Omega_r - Omega_m
+ne0 = (1 - 0.25/2)*2.46e-7*(100.)**3 # in m^-3
+alpha = 1./137 
+lambda_e = 2.426e-10/100. # Compton wavelength of the electron in m
+m_e = 0.511e6 # eV
+thomson_xsec = 6.6524587158e-25/100.**2 # Thomson scattering in m^2
+G = 6.67430e-11*1.6e-19/c**2 # m^3/eV/s^2
+e = np.sqrt(4*np.pi*alpha)
+# e_coulomb = 1.602176634e-19 # Coulombs
+kB = 8.617333262145e-5 # eV/K
+mH=1.67e-27*6.24e18*c**2 # eV
+mHe=6.6464731e-27*6.24e18*c**2 # eV
+
+# see Domcke and Garcia-Cely
+kappa = (16*np.pi*G)**(1/2)/c**2 # s/eV^(1/2)/m^(1/2)
+kappa_converted = kappa/np.sqrt(hbar*c)*0.0195592 # s/(m eV) * eV^2/G (= eV s/(m G))
+# second factor is to convert G to eV^2; above constructed
+# so that kappa^2 B^2 has units of eV^2 s^2/m^2, 
+# matching units of omega^2/c^2 in losc, if B
+# is supplied in Gauss
+        
+Omegab0= phys.omega_baryon
+YHe = phys.nHe*phys.mHe/(phys.nHe*phys.mHe+phys.nH*mH)
+
+
+##################################
+
+# auxiliary functions; may be better to define locally within get_history() later.
+
+def hubble(z): 
+    # in eV. 
+    return phys.hubble(1+z)*hbar
+
+def rho_c(z):  
+    # returns energy density in eV/m^3
+    return 3*hubble(z)**2/(8*np.pi*G)/hbar**2
+
+def nH(z):
+    # returns a number per m^3
+    return (Omegab0*3*(hubble(0)/hbar)**2/(8*np.pi*G))*(1+z)**3/(mH+(mHe*YHe)/(4*(1-YHe)))
+
+def ne(xe,z):
+    # returns a number per m^3
+    return xe*nH(z)
+
+def nHe(z):
+    # rho_b is given by rho_b0*(1+z)^3 and is equal to mH nH + mHe nHe; solve for nHe:
+    # returns a number per m^3
+    return (Omegab0*rho_c(0)*(1+z)**3 - mH*nH(z))/mHe
+
+def chi(z):
+    # aka nHe/nH
+    return nHe(z)/nH(z)
+    
+def plasma_freq(xe,z):
+    # returns a value in eV
+    return np.sqrt(e**2*ne(xe,z)*(hbar*c)**(3)/m_e)
+
+def redshifted_B(B0,z):
+    return B0*(1+z)**2
+    
+def mu(x,xe,z):
+    # Dimensionless.
+    if 1 - np.sqrt(1-plasma_freq(xe,z)**2/(x*TCMB_0*(1+z))**2) >= 1e-13:
+        return np.sqrt(1-plasma_freq(xe,z)**2/(x*TCMB_0*(1+z))**2)
+    else:
+        return 1-plasma_freq(xe,z)**2/(2.*(x*TCMB_0*(1+z))**2) # Taylor expansion (not strictly necessary here)
+
+def losc(x,B0,xe,z): # B0 must be in Gauss
+    # returns a length in units m/s/eV; divide by hbar to get a length in m
+    # no need to do this conversion now though since K12 is in eV s/m
+    if 1 - np.sqrt(1-plasma_freq(xe,z)**2/(x*TCMB_0*(1+z))**2) >= 1e-13:
+        return (2./np.sqrt(((x*TCMB_0*(1+z))**2*(1-mu(x,xe,z))**2)/c**2 + kappa_converted**2*redshifted_B(B0,z)**2*(1+z)**2))
+    else:
+        return (2./np.sqrt(((x*TCMB_0*(1+z))**2*(plasma_freq(xe,z)**2/(2.*(x*TCMB_0*(1+z))**2))**2)/c**2 + kappa_converted**2*redshifted_B(B0,z)**2*(1+z)**2))
+        # Taylor expand 1 - mu if mu is too close to 1
+        
+
+def delta_l(z,delta_l0=3.0857e22):
+    # returns a value in meters (default is delta_l0 = 1 Mpc)
+    return delta_l0/(1+z)
+
+def conversion_rate(x,B0,xe,z,delta_l0=3.0857e22): # B must be in Gauss, delta_l in cm
+    # returns a value in 1/s.  Multiply by hbar to get a value in eV
+    K12 = -1j*np.sqrt(mu(x,xe,z))*kappa_converted*redshifted_B(B0,z)/(1+mu(x,xe,z)) # eV s/m
+    return c*np.abs(K12)**2*losc(x,B0,xe,z)**2/(2*delta_l(z,delta_l0))
+
+def particle_density(xe,z):
+    # returns a number per m^3
+    return nH(z)*(1+xe+chi(z))
+    
+#######################################
+
 def compton_cooling_rate(xHII, xHeII, xHeIII, T_m, rs):
     """Returns the Compton cooling rate.
 
@@ -52,6 +161,7 @@ def get_history(
     reion_switch=False, reion_rs=None,
     photoion_rate_func=None, photoheat_rate_func=None,
     xe_reion_func=None, helium_TLA=False, f_He_ion=None, 
+    GW=None, x0 = None, height = None, width=None,
     mxstep = 1000, rtol=1e-4
 ):
     """Returns the ionization and thermal history of the IGM.
@@ -96,6 +206,14 @@ def get_history(
         Specifies whether to track helium before reionization. 
     f_He_ion : function or float, optional
         f(rs, x_HI, x_HeI, x_HeII) for helium ionization. Treated as constant if float. If None, treated as zero.
+    GW : bool, optional
+        Set to "True" to include effects of gravitational waves on dT_m/dz
+    x0 : float, optional
+        The value at which the GW distribution has its peak
+    height: float, optional
+        The height of the GW spectrum
+    width : float, optional
+        The width of the GW spectrum
     mxstep : int, optional
         The maximum number of steps allowed for each integration point. See *scipy.integrate.odeint* for more information.
     rtol : float, optional
@@ -128,7 +246,20 @@ def get_history(
         struct_bool = True
     else:
         struct_bool = False
-
+    
+    # set GW parameters based on user input.  x0, height and width are set to
+    # some small values if there are no GWs (they will not be used in the calculation)
+    if GW is None or GW == False:
+        GW = False
+        x0 = 1e-12
+        height = 0
+        width = 1e-12
+    elif GW == True:
+        GW = True
+    else:
+        raise TypeError('GW must be True or False (or None).')
+        
+        
     def _f_H_ion(rs, xHI, xHeI, xHeII):
         if baseline_f: 
             return phys.f_std(
@@ -249,23 +380,120 @@ def get_history(
 
     def tla_before_reion(rs, var):
         # Returns an array of values for [dT/dz, dyHII/dz,
-        # dyHeII/dz, dyHeIII/dz].
+        # dyHeII/dz, dyHeIII/dz, dtau/dz].
         # var is the [temperature, xHII, xHeII, xHeIII] inputs.
 
         inj_rate = _injection_rate(rs)
         nH = phys.nH*rs**3
+        
+        ############## Added code for GWs ##############
+        
+        # for computing optical depth:
+        def g_ff(x, T_e, type=None):
+            if type == 'chluba':    
+                g_out = 1. + np.log(1. + np.exp(np.sqrt(3.)/np.pi * (np.log(2.25 / x) + 0.5 * np.log(T_e / m_e)) + 1.425))
+            else:
+                thr = 1e-3
+                def approx_high_x_Draine(x, T_e): 
+                    theta_e = T_e / m_e
+                    return np.log(np.exp(1.) + np.exp(5.960 - np.sqrt(3)/np.pi * np.log(270.783 * x * theta_e**(-0.5))))
+                def approx_low_x_Draine(x, T_e):
+                    theta_e = T_e / m_e
+                    return 4.691 * (
+                        1. - 0.118 * np.log(27.0783 * x * theta_e**(-0.5))
+                    )
+                try:
+                    _ = iter(x)
+                    g_out = np.zeros_like(x)
+                    g_out[x < thr] = approx_high_x_Draine(x[x < thr], T_e[x < thr])
+                    g_out[x >= thr] = approx_high_x_Draine(x[x >= thr], T_e[x >= thr])
+                except:
+                    if x < thr:
+                        g_out = approx_high_x_Draine(x, T_e)
+                    else:
+                        g_out = approx_high_x_Draine(x, T_e)
+            return g_out
+        
+        # for computing optical depth:
+        def Lambda_BR(x,z,T_m,x_e): 
+            n_p = ne0 * (1.+z)**3 * x_e
+            theta_e = T_m / m_e
+            x_electron = x * TCMB_0 * (1. + z) / T_m 
+            return (
+                alpha * lambda_e**3 / (2 * np.pi * np.sqrt(6*np.pi))
+                * n_p * theta_e**(-7./2.)
+                * g_ff(x_electron, T_m, type='chluba') 
+            )
+        
+        # calculate derivative of log of optical depth wrt redshift
+        def dln_tau_ff_dz(tau,z, T_m, xe):
+            x = x0
+            z_to_int = z 
+            x_electron = x * TCMB_0 *(1.+z_to_int) / T_m 
+            Lambda = Lambda_BR(x,z_to_int,T_m,xe)
+            fac_1 = Lambda * (1 - np.exp(-x_electron)) / x_electron**3
+            fac_2 = thomson_xsec * xe * ne0 * (1.+z_to_int)**3 * c / (hubble(z_to_int) / hbar * (1.+z_to_int))
+            return 1./tau * fac_1*fac_2
+        
+        # find dTm/dt due to GWs when the GW spectrum is a delta function
+        # this has the optical depth tau as an input to the function; the diff
+        # eq solver is also solving for tau at each z, so we can use that as an 
+        # input here to find dTm/dt from GWs
+        def dTm_dt_GW_deltaFunc(n_func,z,B0,xe,x0,height,z_ini,tau,T_m,delta_l0=3.0857e22):
 
-        def dlogT_dz(yHII, yHeII, yHeIII, log_T_m, rs):
+            z_pts = np.logspace(np.log10(z),np.log10(z_ini-1))
+            # omega_GW version
+#             y_pts = np.array(list(map(lambda zz: 10**22*(1/hbar)*rho_c(zz)/(1+zz)**3*conversion_rate(x0,B0,zz,delta_l0)*hbar*\
+#                             np.exp(-tau)*dln_tau_ff_dz(tau,zz,T_m)*tau, z_pts)))
+
+            # dn_comov/dx version; n_comov is understood to have units m^-3
+            y_pts = np.array(list(map(lambda zz: height*conversion_rate(x0,B0,xe,zz,delta_l0)*hbar*\
+                            np.exp(-tau)*dln_tau_ff_dz(tau,zz,T_m,xe)*tau*x0*TCMB_0*(1+z), z_pts)))
+            
+            return 2*(1+z)**3/(3*n_func(xe,z))*trapz(y_pts,x=z_pts) # eV^2
+
+        # tophat spectrum - not used
+#         def dTm_dt_GW_tophat(n_func,z,B0,x0,x_width,height,z_ini,tau,T_m,delta_l0=3.0857e22,T0=TCMB_0):
+#             # need to convert the conversion rate to eV, hence *hbar
+#             # returns a value in eV^2.  Divide by hbar*kB to get a value in K/s.
+#             # omega0 in eV
+            
+# #             def x0(z):
+# #                 return omega0/(T0*(1.+z))
+        
+# #             return 2*(1+z)**3/(3*n_func(z))*\
+# #                     dblquad(lambda zz, x: rho_c(zz)/(1+zz)**3*height/x*conversion_rate(x0(zz),B0,zz,delta_l0)*hbar*\
+# #                             np.exp(-tau)*dln_tau_ff_dz(tau,zz,T_m)*tau,z,z_ini,lambda zz: (omega0-omega_width/2)/(T0*(1+zz)), lambda zz: (omega0+omega_width/2)/(T0*(1+zz)))[0]
+           
+#             # dn_comov/dx version
+#             return 2*(1+z)**3/(3*n_func(z))*\
+#                     dblquad(lambda zz, x: height*conversion_rate(x0,B0,zz,delta_l0)*hbar*\
+#                             np.exp(-tau)*dln_tau_ff_dz(tau,zz,T_m)*tau*x*T0*(1+z),z,z_ini,\
+#                             lambda zz: (x0-x_width/2), lambda zz: (x0+x_width/2))[0] # should not redshift in x, redshift invt
+        
+        ############## end added code for GWs ##############
+        
+        def dlogT_dz(yHII, yHeII, yHeIII, log_T_m, tau, rs):
 
             T_m = np.exp(log_T_m)
-
+            
             xe = xHII(yHII) + xHeII(yHeII) + 2*xHeIII(yHeIII)
             xHI = 1 - xHII(yHII)
-            xHeI = chi - xHeII(yHeII) - xHeIII(yHeIII)
+            xHeI = chi - xHeII(yHeII) - xHeIII(yHeIII)        
 
             # This rate is temperature loss per redshift.
             adiabatic_cooling_rate = 2 * T_m/rs
+            
+            # if user has specified GWs are present, add to dlogT_dz the term
+            # computed above.  Otherwise, add 0.
+            if GW:
+                grav_factor = 1./T_m * phys.dtdz(rs) * \
+                (dTm_dt_GW_deltaFunc(particle_density,rs-1,1e-12,xe,x0,height,3e3,tau,T_m))
+                # note I set z_ini = 3000 as opposed to 1000 as we used earlier
 
+            else:
+                grav_factor = 0.
+            
 
             return 1 / T_m * adiabatic_cooling_rate + 1 / T_m * (
                 phys.dtdz(rs)*(
@@ -274,7 +502,7 @@ def get_history(
                     )
                     + _f_heating(rs, xHI, xHeI, xHeII(yHeII)) * inj_rate
                 )
-            )/ (3/2 * nH * (1 + chi + xe))
+            )/ (3/2 * nH * (1 + chi + xe)) + grav_factor
 
 
         def dyHII_dz(yHII, yHeII, yHeIII, log_T_m, rs):
@@ -320,8 +548,8 @@ def get_history(
                 + (1 - phys.peebles_C(xHII(yHII), rs)) * (
                     _f_H_exc(rs, xHI, xHeI, xHeII(yHeII)) * inj_rate
                     / (phys.lya_eng * nH)
-                )
-            )
+                ) + xHI * ne * reion.coll_ion_rate('HI', T_m)
+            ) # added collisional reionization of H
 
         def dyHeII_dz(yHII, yHeII, yHeIII, log_T_m, rs):
 
@@ -379,6 +607,8 @@ def get_history(
                 )
                 + _f_He_ion(rs, xHI, xHeI, xHeII(yHeII)) * inj_rate
                     / (phys.He_ion_eng * nH)
+                # Collisional ionization of HeII to HeIII.
+                - xHeII(yHeII) * ne * reion.coll_ion_rate('HeII', T_m)
             )
 
         def dyHeIII_dz(yHII, yHeII, yHeIII, log_T_m, rs):
@@ -392,26 +622,23 @@ def get_history(
             xe = xHII(yHII) + xHeII(yHeII) + 2*xHeIII(yHeIII)
             ne = xe * nH
 
-            return 0
+            return 2/chi * np.cosh(yHeIII)**2 * phys.dtdz(rs) * (
+                # Collisional ionization of HeII into HeIII.
+                + xHeII(yHeII) * ne * reion.coll_ion_rate('HeII', T_m)
+            )
+        
+        # added log of optical depth as a quantity the solver should calculate at 
+        # each z
+        log_T_m, yHII, yHeII, yHeIII, ln_tau = var[0], var[1], var[2], var[3], var[4]
 
-        log_T_m, yHII, yHeII, yHeIII = var[0], var[1], var[2], var[3]
-
-        # print ([rs, 
-        #     dlogT_dz(yHII, yHeII, yHeIII, log_T_m, rs),
-        #     dyHII_dz(yHII, yHeII, yHeIII, log_T_m, rs),
-        #     dyHeII_dz(yHII, yHeII, yHeIII, log_T_m, rs),
-        #     dyHeIII_dz(yHII, yHeII, yHeIII, log_T_m, rs)
-        # ])
-        # # print(rs, phys.peebles_C(xHII(yHII), rs))
-
-
-        #print(rs, log_T_m, xHII(yHII), xHeII(yHeII), xHeIII(yHeIII))
+        xe = xHII(yHII) + xHeII(yHeII) + 2*xHeIII(yHeIII)
 
         return [
-            dlogT_dz(yHII, yHeII, yHeIII, log_T_m, rs),
+            dlogT_dz(yHII, yHeII, yHeIII, log_T_m, np.exp(ln_tau), rs),
             dyHII_dz(yHII, yHeII, yHeIII, log_T_m, rs),
             dyHeII_dz(yHII, yHeII, yHeIII, log_T_m, rs),
-            dyHeIII_dz(yHII, yHeII, yHeIII, log_T_m, rs)
+            dyHeIII_dz(yHII, yHeII, yHeIII, log_T_m, rs),
+            -dln_tau_ff_dz(np.exp(ln_tau),rs-1,np.exp(log_T_m),xe) ,
         ]
 
     def tla_reion(rs, var):
@@ -426,11 +653,11 @@ def get_history(
         def dlogT_dz(yHII, yHeII, yHeIII, log_T_m, rs):
 
             T_m = np.exp(log_T_m)
-
+            
             xe = xHII(yHII) + xHeII(yHeII) + 2*xHeIII(yHeIII)
             xHI = 1 - xHII(yHII)
             xHeI = chi - xHeII(yHeII) - xHeIII(yHeIII)
-
+            
             # This rate is temperature loss per redshift.
             adiabatic_cooling_rate = 2 * T_m/rs
 
@@ -502,7 +729,7 @@ def get_history(
                     # Photoionization.
                     xHI * photoion_rate_HI(rs)
                     # Collisional ionization.
-                    + xHI * ne * reion.coll_ion_rate('HI', T_m)
+                    + xHI * ne * reion.coll_ion_rate('HI', T_m) # add this to before reionization
                     # Recombination.
                     - xHII(yHII) * ne * reion.alphaA_recomb('HII', T_m)
                 )
@@ -614,14 +841,16 @@ def get_history(
                 phys.Tm_std(rs_start), 
                 phys.xHII_std(rs_start), 
                 phys.xHeII_std(rs_start), 
-                1e-12
+                1e-12,
+                1e-12 # tau
             ]
         else:
             _init_cond = [
                 phys.Tm_std(rs_start), 
                 phys.xHII_std(rs_start), 
                 1e-12, 
-                1e-12
+                1e-12,
+                1e-12 # tau
             ]
     else:
         _init_cond = np.array(init_cond)
@@ -634,11 +863,14 @@ def get_history(
             _init_cond[2] = (1. - 1e-12) * chi
         if init_cond[3] == 0:
             _init_cond[3] = 1e-12
+        if init_cond[4] == 0:
+            _init_cond[4] = 1e-12 # tau
 
     _init_cond[0] = np.log(_init_cond[0])
     _init_cond[1] = np.arctanh(2*(_init_cond[1] - 0.5))
     _init_cond[2] = np.arctanh(2/chi * (_init_cond[2] - chi/2))
     _init_cond[3] = np.arctanh(2/chi *(_init_cond[3] - chi/2))
+    _init_cond[4] = np.log(_init_cond[4])
 
     if reion_rs is None: 
         if photoion_rate_func is None and xe_reion_func is None:
